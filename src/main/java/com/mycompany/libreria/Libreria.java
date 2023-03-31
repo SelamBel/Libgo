f1f7d147882884a11bd205f587b12eb3dbca4c23
SelamBelPC <sydcods@hotmail.es>
2023-03-31 15:52:41 +0200

    1.0.2

--- src/main/java/com/mycompany/libreria/Libreria.java
+++ src/main/java/com/mycompany/libreria/Libreria.java
@@ -7,9 +7,6 @@ import java.util.Scanner;
 
 public class Libreria {
     public static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
-
-    }
     
     public static int getInt(String s, int start, int end) {
 
