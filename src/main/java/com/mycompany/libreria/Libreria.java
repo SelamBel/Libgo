54fd14f2f4a2b63a07ac4e6941a14fcb42eb7dd9
SelamBelPC <sydcods@hotmail.es>
2023-04-24 16:01:42 +0200

    1.1.0

--- src/main/java/com/mycompany/libreria/Libreria.java
+++ src/main/java/com/mycompany/libreria/Libreria.java
@@ -1,18 +1,25 @@
-
-
 package com.mycompany.libreria;
-
 import java.util.Scanner;
 
-
+/**
+ * <h1>Pequeña librería que me facilitará el castigo divino que es programar</h1>
+ * @author SelamBel
+ * @version 1.1.0
+ */
 public class Libreria {
     public static Scanner sc = new Scanner(System.in);
     
-    public static int getInt(String s, int start, int end) {
+    /**
+     * @param texto: texto que aparecerá cada vez que se pida el dato
+     * @param start: el valor mínimo permitido
+     * @param end: el valor máximo permitido
+     * @return el valor que de el usuario
+     */
+    public static int getInt(String texto, int start, int end) {
 
         while (true) {
             int num = start - 1;
-            System.out.println(s);
+            System.out.println(texto);
             String answer = sc.nextLine();
 
             try {
@@ -32,10 +39,44 @@ public class Libreria {
 
     }
     
-    public static int getInt(String s) {
+    /**
+     * @param texto: texto que aparecerá cada vez que se pida el dato
+     * @param start: el valor mínimo permitido
+     * @return el valor que de el usuario
+     */
+    public static int getInt(String texto, int start) {
 
         while (true) {
-            System.out.println(s);
+            int num = start - 1;
+            System.out.println(texto);
+            String answer = sc.nextLine();
+
+            try {
+                num = Integer.parseInt(answer);
+            } catch (Exception e) {
+                System.err.println("ERROR. Debes introducir solo números.");
+                System.out.println("");
+            }
+
+            if (num >= start) {
+                return num;
+            } else {
+                System.err.println("ERROR. Valor fuera de rango");
+                System.out.println("");
+            }
+        }
+
+    }
+
+    /**
+     * @param texto: texto que aparecerá cada vez que se pida el dato
+     * @return el valor que de el usuario
+     */
+    
+    public static int getInt(String texto) {
+
+        while (true) {
+            System.out.println(texto);
             String answer = sc.nextLine();
 
             try {
@@ -47,10 +88,97 @@ public class Libreria {
         }
 
     }
+    
+    /**
+     * @param texto: texto que aparecerá cada vez que se pida el dato
+     * @param start: el valor mínimo permitido
+     * @param end: el valor máximo permitido
+     * @return el valor que de el usuario
+     */
+    
+    public static double getDouble(String texto, int start, int end) {
 
-    public static String getLine(String s) {
         while (true) {
-            System.out.println(s);
+            double num = start - 1;
+            System.out.println(texto);
+            String answer = sc.nextLine();
+
+            try {
+                num = Double.parseDouble(answer);
+            } catch (Exception e) {
+                System.err.println("ERROR. Debes introducir solo números (Seperados por un punto si es decimal)");
+                System.out.println("");
+            }
+
+            if (num >= start && num <= end) {
+                return num;
+            } else {
+                System.err.println("ERROR. Valor fuera de rango");
+                System.out.println("");
+            }
+        }
+
+    }
+    
+    /**
+     * @param texto: texto que aparecerá cada vez que se pida el dato
+     * @param start: el valor mínimo permitido
+     * @return el valor que de el usuario
+     */
+    
+    public static double getDouble(String texto, int start) {
+
+        while (true) {
+            double num = start - 1;
+            System.out.println(texto);
+            String answer = sc.nextLine();
+
+            try {
+                num = Double.parseDouble(answer);
+            } catch (Exception e) {
+                System.err.println("ERROR. Debes introducir solo números (Seperados por un punto si es decimal)");
+                System.out.println("");
+            }
+
+            if (num >= start) {
+                return num;
+            } else {
+                System.err.println("ERROR. Valor fuera de rango");
+                System.out.println("");
+            }
+        }
+
+    }
+    
+    /**
+     * @param texto: texto que aparecerá cada vez que se pida el dato
+     * @return el valor que de el usuario
+     */
+    
+    public static double getDouble(String texto) {
+
+        while (true) {
+            System.out.println(texto);
+            String answer = sc.nextLine();
+
+            try {
+                return Double.parseDouble(answer);
+            } catch (Exception e) {
+                System.err.println("ERROR. Debes introducir solo números.");
+                System.out.println("");
+            }           
+        }
+
+    }
+
+    /**
+     * @param texto: texto que aparecerá cada vez que se pida el dato
+     * @return el texto que de el usuario
+     */
+    
+    public static String getLine(String texto) {
+        while (true) {
+            System.out.println(texto);
             String answer = sc.nextLine();
 
             if (!answer.isBlank() && !answer.isEmpty()) {
